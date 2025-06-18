158846ec0477913c0a20438bcf2a63b5935e8ab9
zippingsugar <zippingsugar@gmail.com>
2025-06-18 13:18:11 +0200

    minor

--- cluster/local_submit_job.sh
+++ cluster/local_submit_job.sh
@@ -16,7 +16,7 @@ cat <<EOT > job.sh
 #SBATCH -n 1
 #SBATCH --cpus-per-task=32
 #SBATCH --gpus=rtx_3090:4
-#SBATCH --time=192:00:00
+#SBATCH --time=143:00:00
 #SBATCH --mem-per-cpu=4048
 #SBATCH --output=$logfile
 #SBATCH --mail-type=END
