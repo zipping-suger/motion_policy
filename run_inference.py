1d99e870940dc090fd345fb64111650ca7cba491
zippingsugar <zippingsugar@gmail.com>
2025-07-19 18:57:10 +0200

    minor

--- run_inference.py
+++ run_inference.py
@@ -30,8 +30,8 @@ GOAL_THRESHOLD = 0.05  # 5cm threshold for goal reaching
 NUM_DMEO = 10
 
 # model_path = "./checkpoints/sdrwmtfu/last.ckpt"
-model_path = "./checkpoints/vtfx7qc8/last.ckpt"
-val_data_path = "./pretrain_data/ompl_cubby_6k"
+model_path = "./checkpoints/0zi5yt83/last.ckpt"
+val_data_path = "./pretrain_data/ompl_table_6k"
 
 # model = PolicyNet().to("cuda:0")
 model = PolicyNet.load_from_checkpoint(model_path).cuda()
